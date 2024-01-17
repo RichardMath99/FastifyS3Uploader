1096ca386feb896ca8a527c4e727f82385719ffc
Richard Matheus <richard.tech99@gmail.com>
2024-01-17 00:18:02 -0300

    add: new endpoint for uploading files

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,4 +1,8 @@
 import fastify from "fastify";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { r2 } from '../lib/cloudflare'
+
 
 const app = fastify()
 
@@ -8,6 +12,19 @@ app.get('/', () => {
     return 'Hello World' 
 })
 
+app.post('/uploads', async () => {
+    const signedUrl = await getSignedUrl(
+        r2,
+        new PutObjectCommand({
+            Bucket: 'rabbithole-dev',
+            Key: 'file.mp4',
+            ContentType: 'video/mp4'
+        }),
+        {expiresIn: 600}
+    )
+
+    return signedUrl;
+})
 app.listen({
     port: 3333,
     host: '0.0.0.0'

4558c83f214ae2a57ec2ee01bf770326573eaafc
Richard Matheus <richard.tech99@gmail.com>
2024-01-18 14:34:41 -0300

    add: requests in api

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,30 +1,51 @@
 import fastify from "fastify";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { randomUUID } from "node:crypto";
 import { r2 } from '../lib/cloudflare'
-
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
 
 const app = fastify()
 
-console.log(process.env.DATABASE_URL)
+const prisma = new PrismaClient()
  
 app.get('/', () => {
     return 'Hello World' 
 })
 
-app.post('/uploads', async () => {
+app.post('/uploads', async (request) => {
+
+    const uploadBodySchema = z.object({
+        name: z.string().min(1),
+        contentType: z.string().regex(/\w+\/[-+.\w]+/),
+    });
+
+    const {name, contentType} = uploadBodySchema.parse(request.body);
+
+    const fileKey = randomUUID().concat('-').concat(name)
+
     const signedUrl = await getSignedUrl(
         r2,
         new PutObjectCommand({
             Bucket: 'rabbithole-dev',
-            Key: 'file.mp4',
-            ContentType: 'video/mp4'
+            Key: fileKey,
+            ContentType: contentType
         }),
-        {expiresIn: 600}
+        {expiresIn: 600},
     )
+
+    await prisma.file.create({
+        data: {
+            name,
+            contentType,
+            key: fileKey,
+        }
+    })
 
     return signedUrl;
 })
+
 app.listen({
     port: 3333,
     host: '0.0.0.0'
